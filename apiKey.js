5b12f1876e3c645494d07d154799f2c085f1be67
Dennis Au <e46mpwr@users.noreply.github.com>
2022-12-20 15:54:48 -0800

    Create apiKey.js

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+ /* Copy this into a file named apiKey.js */
+const apiKey = '1ejyvrJv62CBgzB9haPx3xoBMeKr5JvvFnxsKxgDM';
+const appId = 'io.croquet.dennis.au.microverse';
+export default  {apiKey, appId};
